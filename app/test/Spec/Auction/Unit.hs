494d4887d5ca0d848f88a6cdeb43fb0a05ca7cb8
pdavidow <pdavidow@fastmail.fm>
2022-08-26 07:00:37 -0400

    uhoh
    all tests
      Auction unit
        No bid:                                                                                                              OK (0.51s)
        1 bid just lower than minimal bid:                                                                                   OK (0.34s)
        1 bid at minimal bid, niether registered nor approved:                                                               OK (0.36s)
        1 bid at minimal bid, not registered but yes approved:                                                               FAIL (0.28s)
          Expected funds of W7ce812d to change by
            Value (Map [])
            (excluding 3903 lovelace in fees)
          but they changed by
            Value (Map [(,Map [("",-2000000)]),(6c232f459c409b6fdca382752cb2553cbdede92c84cb5dfa66e6c0b2946c43c7,Map [("token",-1)])])
          Test failed.

--- app/test/Spec/Auction/Unit.hs
+++ app/test/Spec/Auction/Unit.hs
@@ -175,7 +175,7 @@ tests = testGroup "Auction unit"
 
     ,  checkPredicateOptions
         (defaultCheckOptions & (emulatorConfig .~ emCfg))
-        "1 bid at minimal bid, not approved"
+        "1 bid at minimal bid, niether registered nor approved"
         ( assertNoFailedTransactions    
         .&&. walletFundsChange walletSeller mempty
         .&&. walletFundsChange walletBidderA mempty                         
@@ -207,6 +207,49 @@ tests = testGroup "Auction unit"
             Trace.callEndpoint @"close" hSeller closeParams       
             void $ Trace.waitUntilTime $ spDeadline startParams    
             void $ Trace.waitNSlots 5         
+
+
+    ,  checkPredicateOptions
+        (defaultCheckOptions & (emulatorConfig .~ emCfg))
+        "1 bid at minimal bid, not registered but yes approved"
+        ( assertNoFailedTransactions    
+        .&&. walletFundsChange walletSeller mempty
+        .&&. walletFundsChange walletBidderA mempty                         
+        ) $ do
+            hSeller <- Trace.activateContractWallet walletSeller endpoints          
+            hBidderA <- Trace.activateContractWallet walletBidderA endpoints
+
+            let startParams = StartParams 
+                    { spDeadline = TimeSlot.scSlotZeroTime slotCfg + 1_000_000
+                    , spMinBid   = lowestAcceptableBid
+                    , spCurrency = tokenCurrency
+                    , spToken    = tokenName                   
+                    }  
+            Trace.callEndpoint @"start" hSeller startParams   
+            anchor <- getAnchor hSeller 
+            void $ Trace.waitNSlots 5    
+
+            let approveParams = ApproveParams
+                    { apApprovals = [walletPubKeyHash walletBidderA]
+                    , apAnchor = anchor
+                    } 
+            Trace.callEndpoint @"approve" hSeller approveParams                     
+            void $ Trace.waitNSlots 5 
+
+            let bidParams = BidParams
+                    { bpBid    = lowestAcceptableBid
+                    , bpAnchor = anchor
+                    }
+            Trace.callEndpoint @"bid" hBidderA bidParams 
+            void $ Trace.waitNSlots 5         
+
+            let closeParams = CloseParams 
+                    { cpAnchorGraveyard = anchorGraveyard
+                    , cpAnchor = anchor
+                    }                  
+            Trace.callEndpoint @"close" hSeller closeParams       
+            void $ Trace.waitUntilTime $ spDeadline startParams    
+            void $ Trace.waitNSlots 5  
 
 
     ,  checkPredicateOptions
